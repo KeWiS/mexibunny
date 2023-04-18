d18edd2dc9ec5d2e54795a9d53e23a7123205afb
Michał Siwek <mjzsiwek@gmail.com>
2023-04-18 23:15:50 +0200

    Added window startup, configured CMAKELists

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#define SDL_MAIN_HANDLED
+
+#include <SDL.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    auto window = SDL_CreateWindow("MexiBunny", SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
